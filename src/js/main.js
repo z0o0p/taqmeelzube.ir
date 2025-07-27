d12a7af669948dca5a7be7e18cc8eb9a12dba2e7
Taqmeel Zubeir <taqmeelzubeir.dev@gmail.com>
2025-07-27 20:29:28 +0530

    feat(develop): add carousel cards for cta section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+let carouselCurrentCard = 0;
+
 function initializeHeroEffect() {
     const hero = document.querySelector('.hero');
     const nameElement = document.querySelector('.name-gradient');
@@ -12,8 +14,48 @@ function initializeHeroEffect() {
     }
 }
 
+function initializeCarousel() {
+    const passionCard = document.getElementById("passion-card");
+    const expertiseCard = document.getElementById("expertise-card");
+    const interestsCard = document.getElementById("interests-card");
+
+    passionCard.style.display = 'flex';
+    expertiseCard.style.display = 'none';
+    interestsCard.style.display = 'none';
+}
+
+function changeCarouselCurrentSlide(offset) {
+    const passionCard = document.getElementById("passion-card");
+    const expertiseCard = document.getElementById("expertise-card");
+    const interestsCard = document.getElementById("interests-card");
+    const passionDot = document.getElementById("passion-dot");
+    const expertiseDot = document.getElementById("expertise-dot");
+    const interestsDot = document.getElementById("interests-dot");
+
+    carouselCurrentCard = (carouselCurrentCard + offset + 3) % 3;
+
+    passionCard.style.display = 'none';
+    expertiseCard.style.display = 'none';
+    interestsCard.style.display = 'none';
+    passionDot.classList.remove('active');
+    expertiseDot.classList.remove('active');
+    interestsDot.classList.remove('active');
+
+    if (carouselCurrentCard === 0) {
+        passionCard.style.display = 'flex';
+        passionDot.classList.add('active');
+    } else if (carouselCurrentCard === 1) {
+        expertiseCard.style.display = 'flex';
+        expertiseDot.classList.add('active');
+    } else if (carouselCurrentCard === 2) {
+        interestsCard.style.display = 'flex';
+        interestsDot.classList.add('active');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     initializeHeroEffect();
     initializeGame();
+    initializeCarousel()
     document.getElementById("copyright-year").textContent = new Date().getFullYear();
 });
